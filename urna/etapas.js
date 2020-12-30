9b7c5bf191a8a05d5838a56cfdadcb27b03c26ee
Leon Vitor Mansoni Viana <71882720+SSSmashedTomato@users.noreply.github.com>
2020-12-30 20:07:08 -0300

    Add files via upload

---
+++ urna/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '44444',
+                nome: 'Uniqua da Silva',
+                partido: 'Partido dos backyardigans A',
+                fotos: [
+                    {url:'44.png', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '33333',
+                nome: 'Tyrone de Mello',
+                partido: 'Partido dos backyardigans B',
+                fotos: [
+                    {url:'33.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+        },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Pablo Rodrigues',
+                partido: 'Partido dos backyardigans A',
+                vice: 'Power Ranger Vermelho',
+                fotos: [
+                        {url:'11.png', legenda: 'Prefeito'},
+                        {url:'11_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Hippo da Silveira',
+                partido: 'Partido dos backyardigans B',
+                vice: 'Power Ranger Verde',
+                fotos: [
+                        {url:'22.png', legenda: 'Prefeito'},
+                        {url:'22_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+
+            },
+        ]
+    }
+];
